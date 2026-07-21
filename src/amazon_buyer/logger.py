2dc39f711748d3fc2d68cb3d31ab22676b101c54
torak-en <afardona@Gmail.com>
2026-07-21 20:27:51 +0100

    Basic logger implemented

---
+++ src/amazon_buyer/logger.py
@@ -0,0 +1,27 @@
+import logging
+import os
+
+
+def configure_logger():
+
+    os.makedirs("logs", exist_ok=True)
+
+    logger = logging.getLogger("amazon_logger")
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    file_handler = logging.FileHandler("logs/application.log")
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
--- src/amazon_buyer/main.py
+++ src/amazon_buyer/main.py
@@ -1,17 +1,21 @@
 from amazon_buyer.config import Config
 from amazon_buyer.exceptions import ConfigurationError
+from amazon_buyer.logger import configure_logger
 
 def main():
-    print("Monthly Purchase Bot Starting...")
+
+    logger = configure_logger()
+
+    logger.info("Monthly Purchase Bot Starting...")
 
     try:
         config = Config()
 
     except ConfigurationError as error:
-        print(error)
+        logger.error(error)
         return
 
-    print("Configuration loaded successfully")
+    logger.info("Configuration loaded successfully")
 
 
 if __name__ == "__main__":
